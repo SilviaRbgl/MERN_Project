568cf69771c3afdab4c1f2f74ca910f7d653c067
SilviaRbgl <silviarbgl@gmail.com>
2022-11-23 12:24:05 +0100

    server configuration

--- server/server.js
+++ server/server.js
@@ -1,17 +1,51 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import router from "./routes/test.js";
+import * as dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log('Server is running on ' + port + 'port');
-});
 
-import cors from 'cors';
+const addMiddleWares = () => {
+  app.use(express.json());
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(cors());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+
+  const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
+};
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log("Server is running on " + port + "port");
+  });
+};
+
+const loadRoutes = () => {
+  app.use("/api", router);
+};
+
+const mongoDBConnection = async () => {
+  try {
+    await mongoose.connect(process.env.DB);
+    console.log("MongoDB is running in port", port);
+  } catch (error) {
+    console.log("error connecting to MongoDB", error);
+  }
+};
+
+(async function controller() {
+  await mongoDBConnection();
+  addMiddleWares();
+  loadRoutes();
+  startServer();
+})();
