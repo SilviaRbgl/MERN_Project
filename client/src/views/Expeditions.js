4b675018be838eea744c3d385891c1b5d6c5f04d
SilviaRbgl <silviarbgl@gmail.com>
2022-11-24 17:00:32 +0100

    styling cards

--- client/src/views/Expeditions.js
+++ client/src/views/Expeditions.js
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from "react";
-import CardExpedition from "../components/CardExpedition";
 
 function Expeditions() {
-  
   const [expedition, setExpedition] = useState([]);
 
   const getExpeditions = async () => {
     const response = await fetch("http://localhost:5000/api/expeditions/all/");
     const results = await response.json();
-    setExpedition(results.allExpeditions)
+    setExpedition(results.allExpeditions);
     console.log("expeditions >", results);
   };
 
@@ -16,18 +14,21 @@ function Expeditions() {
     getExpeditions();
   }, []);
 
-  return(
-        <div className="bg-slate-50">
-            {expedition.map((expedition, index) => {
-                return <CardExpedition key={index} expedition={expedition} />
-                // return (
-                //     <p>{expedition.island}</p>
-                // )
-            })}
-        </div>
-
-
-  )
+  return (
+    <div className="bg-slate-50 p-10 grid lg:grid-cols-3 gap-10">
+      {expedition.map((expedition) => {
+        return (
+          // <div className="grid lg:grid-cols-3 gap-10">
+          <div className="card">
+            <p className="font-mono font-bold">{expedition.island}</p>
+            <p className="font-mono">{expedition.country}</p>
+            <p className="font-mono">Dates:{expedition.date.beginDate} - {expedition.date.beginDate}</p>
+          </div>
+          // </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Expeditions;
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Home() {
   return (
     <div className='flex-col text-center my-60 mx-12'>
-      <h1 className='font-mono font-bold text-2xl no-underline hover:underline'>remote island expeditions</h1>
+      <NavLink to="/expeditions" className='font-mono font-bold text-2xl no-underline hover:underline'>remote island expeditions</NavLink>
       <p className='font-mono'>logo goes here</p>
     </div>
   )
