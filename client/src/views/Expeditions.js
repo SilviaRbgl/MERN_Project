5690082da8021f6c345f40a9ca99b9addd63b99a
SilviaRbgl <silviarbgl@gmail.com>
2022-11-28 14:02:15 +0100

    detail view of the expeditions

--- client/src/views/Expeditions.js
+++ client/src/views/Expeditions.js
@@ -2,19 +2,26 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 function Expeditions() {
+  
   const [expedition, setExpedition] = useState([]);
 
-  const getExpeditions = async () => {
+  const fetchExpeditions = async () => {
     const response = await fetch("http://localhost:5000/api/expeditions/all/");
     const results = await response.json();
     setExpedition(results.allExpeditions);
     console.log("expeditions >", results);
-    console.log("dates expeditions >", typeof results.allExpeditions[1].date.beginDate);
-    console.log(new Date(results.allExpeditions[1].date.beginDate).toLocaleDateString())
+    console.log(
+      "dates expeditions >",
+      typeof results.allExpeditions[1].date.beginDate
+    );
+    console.log(
+      new Date(results.allExpeditions[1].date.beginDate).toLocaleDateString()
+    );
   };
+  
 
   useEffect(() => {
-    getExpeditions();
+    fetchExpeditions();
   }, []);
 
   const getDates = (date) => {
